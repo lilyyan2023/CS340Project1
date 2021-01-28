c43f1d4e80feb7df3fcda1010316fa867f2c9336
lilyyan2023 <56418644+lilyyan2023@users.noreply.github.com>
2021-01-28 15:39:29 -0600

    server1 WORK

--- http_server1.py
+++ http_server1.py
@@ -9,43 +9,56 @@ def server1(port):
     accept_socket.bind(address)
     #listen
     accept_socket.listen(1)
-    #loop
+    # accept
     while True:
-        #break
         connection_socket, client = accept_socket.accept()
-        data = connection_socket.recv(1024)
-        if len(data) != 0:
-            print(data)
-        else:
-            break
-        #receive byte array，GET request
-
-        # convert it to string
-    data_str = data.decode('utf-8')
-    request = data_str.split(' ')
-    path = request[1]
-            # print(data_str)
-            # if len(data_str) != 0:
-            #     print(data_str)
+        # loop
+        while True:
+            data = connection_socket.recv(1024)
+            # if len(data) != 0:
+            #     print(data)
             # else:
             #     break
-            # connection_socket.sendall(data)
-            # get path
-        # path = data_str[3:]
-        # print(path)
-    try:
-        file = open(path)
-        if path.endswith(".htm") or path.endswith(".html"):
-            connection_socket.send("HTTP/1.1 200 OK\r\n")
-            connection_socket.send("Content-Type: text/html; charset=UTF-8\r\n")
-            connection_socket.send(file.read())
-        else:
-            connection_socket.send("HTTP/1.1 403 Forbidden\r\n")
-    except:
-        connection_socket.send("HTTP/1.1 404 Not Found\r\n")
-    finally:
-        connection_socket.close()
-# server1(8000)
+            print(data)
+            data_str = data.decode('utf-8')
+            if data_str.endswith("\r\n\r\n"):
+            #receive byte array，GET request
+            # convert it to string
+            #     data_str = data.decode('utf-8')
+                request = data_str.split(' ')
+                path = request[1]
+                        # print(data_str)
+                        # if len(data_str) != 0:
+                        #     print(data_str)
+                        # else:
+                        #     break
+                        # connection_socket.sendall(data)
+                        # get path
+                    # path = data_str[3:]
+                    # print(path)
+            try:
+                file = open(path[1:])
+                print(file)
+                if path.endswith(".htm") or path.endswith(".html"):
+                    print("sending 200")
+
+                    connection_socket.send(("HTTP/1.1 200 OK\r\n").encode('utf-8'))
+                    connection_socket.send(("Content-Type: text/html; charset=UTF-8\r\n").encode('utf-8'))
+                    connection_socket.send(file.read().encode('utf-8'))
+                    file.close()
+                else:
+                    print("sending 403")
+                    connection_socket.send(("HTTP/1.1 403 Forbidden\r\n").encode('utf-8'))
+                    file.close()
+            except Exception as e:
+                print(e)
+                print("sending 404")
+                connection_socket.send(("HTTP/1.1 404 Not Found\r\n").encode('utf-8'))
+
+            finally:
+                connection_socket.close()
+                break
+server1(2005)
 
 
 
