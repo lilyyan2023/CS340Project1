98e333560207f127a719288d8f68f815aa52a4a8
Timxuv587 <1430415000@qq.com>
2021-01-29 10:05:21 +0800

    revision

--- http_client.py
+++ http_client.py
@@ -1,6 +1,17 @@
 import socket
 import sys
 
+
+def fetch_data(addr,size):
+    #Cited from https://stackoverflow.com/questions/34192093/python-socket-get
+    sock = socket.socket()
+    sock.connect((socket.gethostbyname(addr),80))
+    sock.send(bytes"GET / HTTP/1.1\r\nHost: "+socket.gethostbyname(addr)+"\r\n\r\n","utf-8"))
+    message = sock.recv(size)
+    sock.close()
+    return(message.decode())
+    
+
 name = str(sys.argv[1])
 output = sys.stdout
 errMsg = sys.stderr
@@ -8,34 +19,23 @@ if name[0:7] != "http://":
     errMsg.write('error')
     sys.exit(1)
 else:
-    sock = socket.socket()
-    sock.connect((name[7:],80))
-    #Cited from https://stackoverflow.com/questions/34192093/python-socket-get
-    sock.send(bytes("GET / HTTP/1.1\r\nHost: "+socket.gethostbyname(name[7:])+"\r\n\r\n","utf-8"))
     #Fetch content length
     size = 1024
-    message = sock.recv(size)
+    response = fetch_data(name[7:],size).split("\r\n")
     header_counter = 0
-    response = message.decode().split("\r\n")
     content_length = -1
     while response[header_counter] != "":
         header_counter += 1
         if(header_counter > len(response)):
             print("should not see")
             size += 1024
-            sock.send(bytes("GET / HTTP/1.1\r\nHost: "+socket.gethostbyname(name[7:])+"\r\n\r\n","utf-8"))
-            message = sock.recv(size)
-            response = message.decode().split("\r\n")
+            response = fetch_data(name[7:],size).split("\r\n")
     for i in range(0,header_counter):
         line = response[i].split(": ")
         if line[0] == "Content-Length":
             content_length = int(line[1])
     #Print entity
-    sock.send(bytes("GET / HTTP/1.1\r\nHost: "+socket.gethostbyname(name[7:])+"\r\n\r\n","utf-8"))
-    message = sock.recv(content_length)
-    response = message.decode().split("\r\n")
-    print(message.decode())
-    #for i in range(header_counter, len(response)-1):
-        #print(response[i])
-    sock.close()
+    response = fetch_data(name[7:],content_length).split("\r\n")
+    for i in range(header_counter, len(response)-1):
+        output.write(response[i])
     sys.exit(0)
